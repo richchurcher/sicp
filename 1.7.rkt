73eca9de032290041a6b05d14c2bb2ebba6db907
Rich Churcher <rich.churcher@gmail.com>
2019-09-06 08:11:39 +1200

    1.7: refactoring good-enough?

---
+++ 1.7.rkt
@@ -0,0 +1,20 @@
+(define (abs x)
+  (if (< x 0) (- x) x))
+(define (square x) (* x x))
+(define (average x y)
+  (/ (+ x y) 2))
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+; (define (good-enough? guess x)
+;   (< (abs (- (square guess) x)) 0.001))
+(define (good-enough? guess old-guess)
+  (< (- (abs guess) (abs old-guess)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? (improve guess x) guess)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
