9cf2806c35f52b2c8be84ff57c29942a103542e8
Rich Churcher <rich.churcher@gmail.com>
2019-09-06 08:47:16 +1200

    Restore old file

--- 1.7.rkt
+++ 1.7.rkt
@@ -1,22 +1,20 @@
 (define (abs x)
   (if (< x 0) (- x) x))
 (define (square x) (* x x))
-
-; x/y2 + 2y
-; ---------
-;     3
-; Racket expression:
-; (/ (+ (/ x (square y)) (* 2 y)) 3)
+(define (average x y)
+  (/ (+ x y) 2))
 (define (improve guess x)
-  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+  (average guess (/ x guess)))
 
-(define (good-enough? old-guess guess)
-  (< (abs (- old-guess guess)) 0.00001))
+; (define (good-enough? guess x)
+;   (< (abs (- (square guess) x)) 0.001))
+(define (good-enough? guess old-guess)
+  (< (- (abs guess) (abs old-guess)) 0.001))
 
-(define (3rt-iter guess x)
-  (if (good-enough? guess (improve guess x))
+(define (sqrt-iter guess x)
+  (if (good-enough? (improve guess x) guess)
       guess
-      (3rt-iter (improve guess x)
+      (sqrt-iter (improve guess x)
                  x)))
-(define (3rt x)
-  (3rt-iter 1.0 x))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
