24fa205974547ef243383b81018534b993646c38
Rich Churcher <rich.churcher@gmail.com>
2019-09-06 08:46:04 +1200

    1.8: cube root (right file this time)

--- 1.8.rkt
+++ 1.8.rkt
@@ -1,20 +1,22 @@
 (define (abs x)
   (if (< x 0) (- x) x))
 (define (square x) (* x x))
-(define (average x y)
-  (/ (+ x y) 2))
+
+; x/y2 + 2y
+; ---------
+;     3
+; Racket expression:
+; (/ (+ (/ x (square y)) (* 2 y)) 3)
 (define (improve guess x)
-  (average guess (/ x guess)))
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
 
-; (define (good-enough? guess x)
-;   (< (abs (- (square guess) x)) 0.001))
-(define (good-enough? guess old-guess)
-  (< (- (abs guess) (abs old-guess)) 0.001))
+(define (good-enough? old-guess guess)
+  (< (abs (- old-guess guess)) 0.00001))
 
-(define (sqrt-iter guess x)
-  (if (good-enough? (improve guess x) guess)
+(define (3rt-iter guess x)
+  (if (good-enough? guess (improve guess x))
       guess
-      (sqrt-iter (improve guess x)
+      (3rt-iter (improve guess x)
                  x)))
-(define (sqrt x)
-  (sqrt-iter 1.0 x))
+(define (3rt x)
+  (3rt-iter 1.0 x))
