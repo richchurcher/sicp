5386e299908e6567611fbb21b8c4308a04e60562
Rich Churcher <rich.churcher@gmail.com>
2019-09-19 08:32:45 +1200

    1.2.2 Tree Recursion

--- scratchpad.rkt
+++ scratchpad.rkt
@@ -1,19 +1,8 @@
-(define (factorial n)
-  (if (= n 1)
-    1
-    (* n (factorial (- n 1)))))
-(factorial 6)
+(define (fib-iter a b count)
+  (if (= count 0)
+    b
+    (fib-iter (+ a b) a (- count 1))))
 
-(define (fact-iter prod i n)
-  (if (> i n)
-    prod
-    (fact-iter (* prod i) (+ 1 i) n)))
-(fact-iter 1 1 6)
+(define (fib n)
+  (fib-iter 1 0 n))
 
-; int fact_iter(int n) {
-;   int prod = 1;
-;   for (int i = 0; i < n; i++) {
-;     prod = (i + 1) * prod;
-;   }
-;   return prod;
-; }
